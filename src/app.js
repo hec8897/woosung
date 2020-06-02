3c8fe16c1158e3420945c3fbbc8e38ecb5b17d35
dawoonkim <hec8897@gmail.com>
2020-06-02 15:41:16 +0900

    메인 슬라이드 영역 추가

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import './common.scss'
 import AppHeader from './common/header'
 import AppFooter from './common/footer'
 import router from './router'
+import VueCarousel from 'vue-carousel';
+Vue.use(VueCarousel);
 new Vue({
     router,
     template:`<div>
--- src/main/slider.js
+++ src/main/slider.js
@@ -1,5 +1,22 @@
+import { Carousel, Slide } from 'vue-carousel';
 const MainSlider = {
-    template:`<div class='slider'>슬라이더</div>`
+    template:`<div class='slider'>
+            <carousel :perPage='1'>
+            <slide>
+                <p>Slide 1 Content</p>
+            </slide>
+            <slide>
+                <p>Slide 2 Content</p>
+            </slide>
+            <slide>
+                <p>Slide 3 Content</p>
+            </slide>
+            </carousel>
+    </div>`,
+    components: {
+        Carousel,
+        Slide
+      }
 }
 
 export default MainSlider;
